b1846a539954b1bfc35747dccedb15e0c08d09be
Uladzimir Kameniukou <6579080@gmail.com>
2021-06-30 19:12:32 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    static double func(){
+        double result = Math.tan(1);
+        System.out.println(result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        func();
+
+
+    }
+}
